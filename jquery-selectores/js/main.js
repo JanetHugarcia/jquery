fbf745dd07cf09a3736c63a2d4ae647f300a771f
JanetHugarcia <nayavz@gmail.com>
2016-05-11 08:43:34 -0500

    solo era actualizar x.x

--- jquery-selectores/js/main.js
+++ jquery-selectores/js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
-	var string=("#principal").val();
-
-	$("#nexted").click(function(){
-		alert(string);
-		// $().addClass('highlight').animate({ marginLeft: 10}, 'slow');
+	$('#nexted').click(function(){
+		var string=$('#principal').val();
+		$(string).addClass('highlight').animate({ marginLeft: 10}, 'slow');
 	});
 });
