4d727db2a36e269c7d04724863d7fc0ca75e3f73
JanetHugarcia <nayavz@gmail.com>
2016-05-11 08:30:24 -0500

    creando archivos

---
+++ jquery-selectores/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	var string=("#principal").val();
+
+	$("#nexted").click(function(){
+		alert(string);
+		// $().addClass('highlight').animate({ marginLeft: 10}, 'slow');
+	});
+});
