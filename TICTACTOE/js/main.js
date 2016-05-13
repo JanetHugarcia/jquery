59fad0b114917ff36ed578545aa3444bcb9db416
janetHugarcia <janet.huacahuasi.garcia@gmail.com>
2016-05-12 22:00:19 -0500

    95% tictactoe

--- TICTACTOE/js/main.js
+++ TICTACTOE/js/main.js
@@ -4,67 +4,80 @@ $(document).ready(function(){
 		var claseFila =  i;
 		$('#padre').append("<div class='" + claseFila + "'></div>");
 		 for(var j=1;j<=3;j++){
-				$("." + claseFila).append("<button class='btn' id='" + claseFila+j + "'>"+"</button>");		 
+				$("." + claseFila).append("<button class='btn boton' id='" + claseFila+j + "'>"+"</button>");		 
 		};	
+			
 	}
 	var c = 0;
-	
-	$('.btn').click(function(){
+	$('.boton').text("+");
+	$('.boton').click(function(){
 		c++;
 		if(c % 2){
 			$(this).each(function(){
-				$(this).text("x");
+				$(this).text("x").css('color','blue');
 				$(this).attr("disabled",true);
 			})
 		}else{
 			$(this).each(function(){
-				$(this).text("o");
+				$(this).text("o").css('color','red');;
 				$(this).attr("disabled",true);
 				
 			})
 		}
 
-		if( $('#11').text()!="" && $('#11').text()==$('#22').text()  && $('#11').text()==$('#33').text()){
-				alert($('#11').text() +" Ganaste");
+		if( $('#11').text()!="+" && $('#11').text()==$('#22').text()  && $('#11').text()==$('#33').text()){
+				alert(" Ganó: "+$('#11').text() );
+				$('.boton').attr("disabled",true);
 		}
-		if( $('#13').text()!="" && $('#13').text()==$('#22').text()  && $('#13').text()==$('#31').text()){
-				alert($('#13').text() +" Ganaste");
+		if( $('#13').text()!="+" && $('#13').text()==$('#22').text()  && $('#13').text()==$('#31').text()){
+				alert(" Ganó: "+$('#13').text() );
+				$('.boton').attr("disabled",true);
 		}
-		if( $('#11').text()!="" && $('#11').text()==$('#12').text()  && $('#11').text()==$('#13').text()){
-				alert($('#11').text() +" Ganaste");
+		if( $('#11').text()!="+" && $('#11').text()==$('#12').text()  && $('#11').text()==$('#13').text()){
+				alert(" Ganó: "+$('#11').text() );
+				$('.boton').attr("disabled",true);
 		}
-		if( $('#11').text()!="" && $('#11').text()==$('#21').text()  && $('#11').text()==$('#31').text()){
-				alert($('#11').text() +" Ganaste");
+		if( $('#11').text()!="+" && $('#11').text()==$('#21').text()  && $('#11').text()==$('#31').text()){
+				alert(" Ganó: "+$('#11').text() );
+				$('.boton').attr("disabled",true);
 		}
-		if( $('#12').text()!="" && $('#12').text()==$('#22').text()  && $('#12').text()==$('#32').text()){
-				alert($('#12').text() +" Ganaste");
+		if( $('#12').text()!="+" && $('#12').text()==$('#22').text()  && $('#12').text()==$('#32').text()){
+				alert(" Ganó: "+$('#12').text() );
+				$('.boton').attr("disabled",true);
+
 		}
-		if( $('#13').text()!="" && $('#13').text()==$('#23').text()  && $('#13').text()==$('#33').text()){
-				alert($('#13').text() +" Ganaste");
+		if( $('#13').text()!="+" && $('#13').text()==$('#23').text()  && $('#13').text()==$('#33').text()){
+				alert(" Ganó: "+$('#13').text() );
+				$('.boton').attr("disabled",true);
 		}
-		if( $('#21').text()!="" && $('#21').text()==$('#22').text()  && $('#21').text()==$('#23').text()){
-				alert($('#21').text() +" Ganaste");
+		if( $('#21').text()!="+" && $('#21').text()==$('#22').text()  && $('#21').text()==$('#23').text()){
+				alert(" Ganó: "+$('#21').text() );
+				$('.boton').attr("disabled",true);
 		}
-		if( $('#31').text()!="" && $('#31').text()==$('#32').text()  && $('#31').text()==$('#33').text()){
-				alert($('#31').text() +" Ganaste");
+		if( $('#31').text()!="+" && $('#31').text()==$('#32').text()  && $('#31').text()==$('#33').text()){
+				alert(" Ganó: "+$('#31').text() );
+				$('.boton').attr("disabled",true);
 		}
 		// var temp = $('#11').text();
 		// var esigual=true;
-
-		// for (var n=1;n<=3;n++){
-		// 	if(temp == $('#1'+n).text()){
-		// 		esigual=false;
+		// for(var m =1;m<=3;m++){
+		// 	for(var n=1;n<=3;n++){
+		// 		 // $('#'+m.toString()+n.toString()).hide();
+		// 		 if(m==n){
+		// 		 	$('#'+m.toString()+n.toString()).text("u");
+		// 		 	 // alert(m.toString()+n.toString()+"\n");
+		// 		 }
+				
 		// 	}
 		// }
-
-		// 	if(esigual==false){
-		// 		alert($('#11').text() + "Ganaste");
-		// 	}
 		
 
 	});
 
-	
+	$('.btnNew').click(function(){
+		$('.boton').text("+").css('color','black');
+		$('.boton').attr("disabled",false);
+	});
 	
 
 
